ebd349f34deaf780d6cdc1ecf78afadd3b778d33
Ilenia96 <ilenianese96@gmail.com>
2021-10-08 16:55:40 +0200

    change vue and style

---
+++ src/app/shared/components/footer/footer-nav-links/links.ts
@@ -0,0 +1,119 @@
+import { ILink } from "./ILink";
+
+export const buyProducts : ILink[] = [
+  {name: 'Mac' , url: ''},
+  {name: 'iPad' , url: ''},
+  {name: 'iPhone' , url: ''},
+  {name: 'Watch' , url: ''},
+  {name: 'TV' , url: ''},
+  {name: 'Music' , url: ''},
+  {name: 'AirPods' , url: ''},
+  {name: 'iPod touch' , url: ''},
+  {name: 'AirTag' , url: ''},
+  {name: 'Accessori' , url: ''},
+  {name: 'Carte regalo' , url: ''}
+
+]
+
+export const appleServices : ILink[] = [
+  {name: 'Apple Music' , url: ''},
+  {name: 'Apple TV+' , url: ''},
+  {name: 'Apple Fitness+' , url: ''},
+  {name: 'Apple Arcade' , url: ''},
+  {name: 'iCloud' , url: ''},
+  {name: 'Apple One' , url: ''},
+  {name: 'Apple Pay' , url: ''},
+  {name: 'Apple Books' , url: ''},
+  {name: 'Apple Podcast' , url: ''},
+  {name: 'App Store' , url: ''}
+
+]
+
+export const account : ILink[] = [
+  {name: 'Gestisci il tuo ID Apple' , url: ''},
+  {name: 'Account Apple Store' , url: ''},
+  {name: 'iCloud.com' , url: ''}
+
+]
+
+export const appleStore : ILink[] = [
+{name: 'Trova uno Store' , url: ''},
+{name: 'Acquista online' , url: ''},
+{name: 'Genius Bar' , url: ''},
+{name: 'Today at Apple' , url: ''},
+{name: 'Campo Estivo Apple' , url: ''},
+{name: `L'app Apple Store`, url: ''},
+{name: 'Ricondizionati e fine serie' , url: ''},
+{name: 'Finanziamenti' , url: ''},
+{name: 'Apple Trade In' , url: ''},
+{name: `Stato dell'ordine` , url: ''},
+{name: 'Assistenza agli acquisti' , url: ''}
+
+]
+
+
+export const business : ILink[] = [
+  {name: 'Apple per il business' , url: ''},
+  {name: 'Acquisti per le aziende' , url: ''}
+
+
+]
+
+export const education : ILink[] = [
+  {name: `Apple nell'istruzione` , url: ''},
+  {name: `Acquisti per l'università` , url: ''}
+]
+
+export const health : ILink[] = [
+  {name: 'Apple e la salute' , url: ''},
+  {name: 'Salute su Apple Watch' , url: ''}
+]
+
+export const appleValues : ILink[] = [
+  {name: 'Accessibilità' , url: ''},
+  {name: 'Ambiente' , url: ''},
+  {name: 'Privacy' , url: ''},
+  {name: 'Responsabilità dei fornitori' , url: ''}
+]
+
+export const info : ILink[] = [
+  {name: 'Newsroom' , url: ''},
+  {name: 'Leadership Apple' , url: ''},
+  {name: 'Opportunità di lavoro' , url: ''},
+  {name: 'Garanzia' , url: ''},
+  {name: 'Investitori' , url: ''},
+  {name: 'Etica e rispetto delle norme' , url: ''},
+  {name: 'Eventi' , url: ''},
+  {name: 'Posti di lavoro in Europa' , url: ''},
+  {name: 'Contatti' , url: ''},
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
