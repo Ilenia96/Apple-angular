7096443ca95f4bc08e049570e5fad5c24cf39912
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-08 14:46:09 +0200

    add transition effect

--- src/app/shared/components/header/discount-device/discount-device.component.ts
+++ src/app/shared/components/header/discount-device/discount-device.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { faGrinTongueSquint } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-discount-device',
@@ -10,10 +11,28 @@ export class DiscountDeviceComponent implements OnInit {
   @Input() discount !: boolean;
   @Input() discountText !: string;
   @Input() url !: string;
+  bottom = "0px";
+  zIndex = "-1";
+  bgcolor = "#0071E3";
+  textColor = "white";
+  linkColor = "white";
 
-  constructor() { }
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.bottom = "-35px";
+      this.zIndex = "1";
+      console.log(`ciao`);
+    }, 0)
+    setTimeout(() => {
+      this.bgcolor = "#f5f5f7";
+      this.textColor = "#6e6e70";
+      this.linkColor = "#0066CB";
+    }, 1500)
   }
 
 }
