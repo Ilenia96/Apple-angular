96f52f50cf82e0b0ce6e91bbf1920423f931648f
Paolo Massimo Veneziani <paolo.veneziani@hotmail.com>
2021-10-15 11:06:14 +0200

    fix optional chaining

--- src/app/shared/services/MacDataService.ts
+++ src/app/shared/services/MacDataService.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable} from "rxjs";
-import {tap} from "rxjs/operators"
+import {map, tap} from "rxjs/operators"
 import { IHeader, IHeaderNavbar } from "src/app/models/Header";
 import { IMain } from "src/app/models/Main";
 
@@ -16,8 +16,8 @@ export class MacDataService {
 
     }
 
-    getHeader(): Observable<IHeader[]> {
-        return this.httpClient.get<IHeader[]>('http://localhost:3000/header');
+    getHeader(): Observable<IHeaderNavbar> {
+        return this.httpClient.get<IHeader[]>('http://localhost:3000/header').pipe(map((data) => data[0]?.headerNavbar))
     }
 
     getMain(): Observable<IMain[]> {
